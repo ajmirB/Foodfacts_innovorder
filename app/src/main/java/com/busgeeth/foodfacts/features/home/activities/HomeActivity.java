80365c1cf6c546d4834a04dffddcc7c65cb22e13
Ajmir Busgeeth <ajmirulla.busgeeth@shopmium.com>
2017-10-10 13:43:19 +0200

    Implement user general interface of the project

--- app/src/main/java/com/busgeeth/foodfacts/features/home/activities/HomeActivity.java
+++ app/src/main/java/com/busgeeth/foodfacts/features/home/activities/HomeActivity.java
@@ -1,7 +1,17 @@
 package com.busgeeth.foodfacts.features.home.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.busgeeth.foodfacts.R;
 import com.busgeeth.foodfacts.features.commons.BaseActivity;
 
 
 public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
 }
--- app/src/main/java/com/busgeeth/foodfacts/features/home/fragments/FoodFactDetailFragment.java
+++ app/src/main/java/com/busgeeth/foodfacts/features/home/fragments/FoodFactDetailFragment.java
@@ -1,7 +1,20 @@
 package com.busgeeth.foodfacts.features.home.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.busgeeth.foodfacts.R;
 import com.busgeeth.foodfacts.features.commons.BaseFragment;
 
 
 public class FoodFactDetailFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_foodfact_detail, container, false);
+        return view;
+    }
 }
