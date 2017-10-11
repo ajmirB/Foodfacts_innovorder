6675636d23bfe9cd3c193ae3b40308a2e8df29f8
Ajmir Busgeeth <ajmirulla.busgeeth@shopmium.com>
2017-10-11 16:48:21 +0200

    - Add request logging - Fix SSL handshakes when launched on API21+ devices

--- app/src/main/java/com/busgeeth/foodfacts/core/network/RestClient.java
+++ app/src/main/java/com/busgeeth/foodfacts/core/network/RestClient.java
@@ -1,5 +1,12 @@
 package com.busgeeth.foodfacts.core.network;
 
+import java.util.Collections;
+
+import okhttp3.CipherSuite;
+import okhttp3.ConnectionSpec;
+import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -12,8 +19,23 @@ public class RestClient {
     }
 
     private static Retrofit getRestClient() {
+        ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                .tlsVersions(TlsVersion.TLS_1_2)
+                .cipherSuites(
+                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                        CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256)
+                .build();
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor)
+                .connectionSpecs(Collections.singletonList(spec))
+                .build();
+
         return new Retrofit.Builder()
                 .baseUrl("https://world.openfoodfacts.org/api/v0/")
+                .client(client)
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
